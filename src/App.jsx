72de36f063f8b3135baf386f71dd231049b096ff
precious onanuga <preciousonanuga3@gmail.com>
2025-10-05 13:06:25 +0100

    Instaling initial setup with React and Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <h2 className='text-blue-500'>TravelPadi</h2>
+        <div className='flex  bg-amber-500 p-2 rounded'>
+          <button className='px-10'>Search</button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default App
