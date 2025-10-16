cd26af0f83504f4ef302c1760824acd182e88ad9
precious onanuga <preciousonanuga3@gmail.com>
2025-10-16 11:43:04 +0100

    feat: improving the landing page (destinationpage)

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
+import DestinationCard from '../components/DestinationCard';
 
 export default function Destinations() {
   return (
     <>
-      <div className='bg-amber-100 container max-w-none w-full text-center mx-auto p-10'>
-        <h1 className='text-4xl p-5 text-amber-600 '>Discover Amazing Destinations</h1>
-        <p >
-          Search through popular destinations worldwide and add them to your personalized itinerary.
-          Each destination includes top attractions, best visiting times, and local insights.
-        </p>
+      <div className='bg-amber-100 container max-w-none w-full text-center mx-auto p-6 '>
+        <h1 className='text-4xl shadow-* text-shadow-md font-medium p-5 text-amber-600 '>Discover Amazing Destinations</h1>
+        <div className='mx-auto justify-center p-3 w-180  '>
+          <p className='font-medium'>Search through popular destinations worldwide and add them to your personalized itinerary. Each destination includes top attractions, best visiting times, and local insights.</p>
+        </div>
         
-        <label htmlFor='search-destination' className='bg-cyan-950'></label>
         <input
           type='text'
-          value={""} 
-          placeholder='Search for destinations'
-          className='flex items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'
+          placeholder='🔍 Search for destinations'
+          className='w-full flex p-2 bg-amber-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-blye-500'
         />
 
+        <DestinationCard />
       </div>
 
     </>
