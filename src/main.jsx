e16330ed61d0a7445f6eafab1e038834405147c5
precious onanuga <preciousonanuga3@gmail.com>
2025-10-23 16:37:00 +0100

    feat: refactoring the React.strictMode for better developer experience; detecting bugs early on

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
